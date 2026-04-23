ec916ecdcdde91929159550a8ddf244d4f8e7642
ciradiciradi-stack <ciradiciradi@gmail.com>
2026-04-23 17:16:21 -0400

    Update casosDePrueba.java agrego casos de prueba del 5 al 7

--- src/main/java/Framework/casosDePrueba.java
+++ src/main/java/Framework/casosDePrueba.java
@@ -12,8 +12,37 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class casosDePrueba extends configuracionBase
 {
-    @Test @Order(1)
-    public void CasoDePrueba1() throws Exception {
+    @Test @Order(5)
+    public void ValidarCarro() throws Exception {
         // Todo...
+        funcionesDeNegocio.Login("standard_user","secret_sauce");
+        accionesBase.Click("//*[@id='add-to-cart-sauce-labs-backpack']");
+
+        accionesBase.Click(objetosDePagina.Catalogo_Carrito);
+        accionesBase.CompararTexto(objetosDePagina.Carrito_Titulo, TitYourCart, "La página no corresponde a la del carro");
+        List<WebElement> links =Driver.findElements(By.xpath("//*[@id='cart_contents_container']/div/div[1]/div[3]"));
+
+// Iterar sobre los elementos
+        for (WebElement link : links) {
+            if (!link.getText().isEmpty()) {
+            System.out.println("Número de productos: " + links.size());
+            } else System.out.println("No hay elementos");
+        }
+        accionesBase.Click(objetosDePagina.Carrito_Btn_CheckOut);
     }
+    @Test @Order(6)
+    public void InformacionCompra() throws Exception {
+        accionesBase.CompararTexto(objetosDePagina.CheckOut_Titulo, TitCheckoutInformation, "No esta en el CheckOut");
+        accionesBase.Escribir(objetosDePagina.CheckOut_AddName, CheckOutTxtAddName);
+        accionesBase.Escribir(objetosDePagina.CheckOut_AddLastName, CheckOutTxtAddLastName);
+        accionesBase.Escribir(objetosDePagina.CheckOut_AddPostalCode, CheckOutTxtAddPostalCode);
+        accionesBase.Click(objetosDePagina.CheckOut_BtnContinue);
+    }
+    @Test @Order(7)
+    public void InformacionCheckOut() throws Exception {
+        accionesBase.CompararTexto(objetosDePagina.OverView_Titulo, TitCheckoutOverview, "No esta en el OverView");
+        System.out.println(accionesBase.LeerTexto(objetosDePagina.OverView_SummaryPayment));
+        System.out.println(accionesBase.LeerTexto(objetosDePagina.OverView_ShippingInfo));
+    }
+
 }
